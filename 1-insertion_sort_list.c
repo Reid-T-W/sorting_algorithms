13ba2eadfdbe2f91df5213e9ca5cc71f63c155fa
Reid-T-W <rediettadesse100@gmail.com>
2022-08-16 19:14:38 +0300

    Modifaction of assignment of declared variables

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 /**
  * insertion_sort_list - sorts a doubly linked list using insertion sort
  * @list: linked list to be sorted
@@ -6,37 +7,67 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *x;
+	listint_t *x = *list;
 	listint_t *x2;
 	listint_t *x3;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-
-	for (x3 = (*list)->next; x3; x3 = x3->next)
+	if ((list == NULL) || (*list == NULL) || ((*list)->next == NULL))
 	{
-		x = x3;
+		return;
+	}
+		x = x->next;
+		x3 = x->next;
 		x2 = x->prev;
-		while (x2 && x2->n > x->n)
+	while (x != NULL)
+	{
+		while (x2 != NULL)
 		{
-			if (x->next)
+			if (x2->n > x->n)
 			{
-				x->next->prev = x2;
+				swap(list, x2, x);
+				x2 = x;
+				print_list(*list);
 			}
-			x2->next = x->next;
-			x->next = x2;
-			x->prev = x2->prev;
-			if (x2->prev)
-			{
-				x2->prev->next = x;
-			}
-			else
-			{
-				*list = x;
-			}
-			x2->prev = x;
-			print_list(*list);
+			x2 = x2->prev;
+		}
+		x = x3;
+		if (x != NULL)
+		{
+			x3 = x->next;
 			x2 = x->prev;
+		}
+	}
+}
+
+/**
+ * swap - This function swaps two nodes
+ * @list: linked list
+ * @s: node to swap with x
+ * @x: node to swap with s
+ * Return: void
+ */
+void swap(listint_t **list, listint_t *s, listint_t *x)
+{
+	if (s != x)
+	{
+		x->prev->next = x->next;
+		if (x->next != NULL)
+		{
+			x->next->prev = x->prev;
+		}
+		if (s->prev == NULL)
+		{
+			s->prev = x;
+			x->next = s;
+			x->prev = NULL;
+			*list = x;
+		}
+		else
+		{
+			x->prev = s->prev;
+			s->prev->next = x;
+			x->next = s;
+			s->prev = x;
 		}
 	}
 }
