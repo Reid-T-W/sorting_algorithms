61c1482b3abb35e5847fbc43cd09796d67e9c820
Reid-T-W <rediettadesse100@gmail.com>
2022-08-17 00:22:27 +0300

    Counting sort algorithim

---
+++ 102-counting_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *counting_sort - sorts an array using counting sort algo
+ *@array: array to be sorted
+ *@size: size of array
+ *Return: void
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i;
+	int j;
+	int *count_array;
+	int *index;
+	int *sort_array;
+	int k = array[0];
+
+	if (size < 2)
+		return;
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > k)
+			k = array[i];
+	}
+	count_array = malloc(sizeof(int) * (k + 1));
+	for (i = 0; i < size; i++)
+	{
+		index = count_array + array[i];
+		*index = *index + 1;
+	}
+	for (j = 0; j < k + 1; j++)
+	{
+		index = count_array + j;
+		*(index + 1) = *index + *(index + 1);
+	}
+	sort_array = malloc(sizeof(int) * size);
+	print_array(count_array, k + 1);
+	for (i = 1; i <= size; i++)
+	{
+		count_array[array[size - i]] = count_array[array[size - i]] - 1;
+		sort_array[count_array[array[size - i]]] = array[size - i];
+	}
+}
